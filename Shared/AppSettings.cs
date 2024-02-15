c37c562363e7309ba730894ec31cc3a10d9b63bd
Vitor Moschetta <vitor.moschetta@mercadolivre.com>
2024-02-15 01:11:12 -0400

    change configs

--- Shared/AppSettings.cs
+++ Shared/AppSettings.cs
@@ -10,6 +10,4 @@ public class MongoDbSettings
     public string ConnectionString { get; set; } = "mongodb://localhost:27017";
     public string DatabaseName { get; set; } = "dotnet-mongodb";
     public string CollectionName { get; set; } = "todos";
-    public string Username { get; set ; } = "root";
-    public string Password { get; set; } = "example";
 }
